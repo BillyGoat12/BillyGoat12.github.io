cacfd16406a30f4f7eca5afd8da4aa118a069a5e
Ubuntu <ubuntu@ip-172-31-54-206.ec2.internal>
2020-06-11 18:05:43 +0000

    underpants

--- studies/scopes.js
+++ studies/scopes.js
@@ -8,6 +8,9 @@
 *
 *
 *
-* Scopes - 
-* 
+* Scopes - scope refers to what variables or constants are accessible. There are
+* basically two type of scope which are global and local.
+*
 */
+
+// Global scope -
